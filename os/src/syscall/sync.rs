476cd81b80b1a60df3aa92440423e6f9df64f4e8
Linwei Zhang <lewiszlw520@gmail.com>
2024-11-04 15:36:33 +0800

    Update

--- os/src/syscall/sync.rs
+++ os/src/syscall/sync.rs
@@ -201,6 +201,7 @@ pub fn sys_semaphore_up(sem_id: usize) -> isize {
     let sem = Arc::clone(process_inner.semaphore_list[sem_id].as_ref().unwrap());
     process_inner.seamphore_available[sem_id] += 1;
     process_inner.seamphore_allocation[tid].as_mut().unwrap()[sem_id] -= 1;
+    process_inner.seamphore_need[tid].as_mut().unwrap()[sem_id] += 1;
     drop(process_inner);
     sem.up();
     0
@@ -224,7 +225,22 @@ pub fn sys_semaphore_down(sem_id: usize) -> isize {
 
     process_inner.seamphore_need[tid].as_mut().unwrap()[sem_id] += 1;
 
+
     if process_inner.enable_deadlock_detect {
+        // request <= need
+        if 1 > process_inner.seamphore_need[tid].as_ref().unwrap()[sem_id] {
+            return -0xDEAD;
+        }
+
+        // request <= available
+        if 1 > process_inner.seamphore_available[sem_id] {
+            return -0xDEAD;
+        }
+
+        process_inner.seamphore_available[sem_id] -= 1;
+        process_inner.seamphore_allocation[tid].as_mut().unwrap()[sem_id] += 1;
+        process_inner.seamphore_need[tid].as_mut().unwrap()[sem_id] -= 1;
+
         let mut work = process_inner.seamphore_available.clone();
         let mut finish = vec![false; process_inner.tasks.len()];
         for (tid, task) in process_inner.tasks.iter().enumerate() {
@@ -242,6 +258,7 @@ pub fn sys_semaphore_down(sem_id: usize) -> isize {
             }
         }
         if finish.iter().any(|f| !f) {
+            process_inner.seamphore_need[tid].as_mut().unwrap()[sem_id] -= 1;
             return -0xDEAD;
         }
     }
@@ -250,11 +267,6 @@ pub fn sys_semaphore_down(sem_id: usize) -> isize {
     drop(process_inner);
     sem.down();
 
-    let process = current_process();
-    let mut process_inner = process.inner_exclusive_access();
-    process_inner.seamphore_need[tid].as_mut().unwrap()[sem_id] -= 1;
-    process_inner.seamphore_allocation[tid].as_mut().unwrap()[sem_id] += 1;
-    process_inner.seamphore_available[sem_id] -= 1;
     0
 }
 /// condvar create syscall
